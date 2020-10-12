1424fb87cd3beedb8472d1b312e22b4144862e8c
kyouheinagano <72237786+kyouheinagano@users.noreply.github.com>
2020-10-12 22:46:04 +0900

    点数計算の追加
    スコアに当たった時の点数を追加

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private GameObject scoreText;
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreText = GameObject.Find("ScoreText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(tag);
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag" || other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 30;
+        }
+        this.scoreText.GetComponent<Text>().text = "Score"+ score;
+    }
+}
